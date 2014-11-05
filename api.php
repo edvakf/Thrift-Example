d1972316d223e2cdcaa3916a297d130b29866fde
Atsushi Takayama <taka.atsushi@gmail.com>
2014-11-05 18:14:14 +0900

    add example code to hit the api

---
+++ api.php
@@ -0,0 +1,60 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/gen-php/My/Example/Types.php';
+require __DIR__.'/gen-php/My/Example/ArithmeticService.php';
+
+use Thrift\Transport\TBufferedTransport;
+use Thrift\Transport\TPhpStream;
+use Thrift\Protocol\TJsonProtocol;
+
+class ArithmeticServiceImpl implements \My\Example\ArithmeticServiceIf
+{
+    public function add(\My\Example\Complex $i1, \My\Example\Complex $i2)
+    {
+        return new \My\Example\Complex([
+            'real' => $i1->real + $i2->real,
+            'imaginary' => $i1->imaginary + $i2->imaginary,
+        ]);
+    }
+
+    public function subtract(\My\Example\Complex $i1, \My\Example\Complex $i2)
+    {
+        return new \My\Example\Complex([
+            'real' => $i1->real - $i2->real,
+            'imaginary' => $i1->imaginary - $i2->imaginary,
+        ]);
+    }
+
+    public function multiply(\My\Example\Complex $i1, \My\Example\Complex $i2)
+    {
+        return new \My\Example\Complex([
+            'real' => $i1->real * $i2->real,
+            'imaginary' => $i1->imaginary * $i2->imaginary,
+        ]);
+    }
+
+    public function divide(\My\Example\Complex $i1, \My\Example\Complex $i2)
+    {
+        return new \My\Example\Complex([
+            'real' => $i1->real / $i2->real,
+            'imaginary' => $i1->imaginary / $i2->imaginary,
+        ]);
+    }
+}
+
+
+header('Content-Type: application/x-thrift');
+
+$handler   = new ArithmeticServiceImpl();
+$processor = new \My\Example\ArithmeticServiceProcessor($handler);
+
+$transport = new TBufferedTransport(new TPhpStream(TPhpStream::MODE_R | TPhpStream::MODE_W));
+
+$protocol = new TJsonProtocol($transport, true, true);
+
+$transport->open();
+$processor->process($protocol, $protocol);
+$transport->close();
+
+
